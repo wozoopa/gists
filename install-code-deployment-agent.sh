7ac94e20a3346a0b25595c4e5c928dc86a2222fe
user <user@localhost>
2018-07-26 07:29:30 -0500

    Added snippet for installing code-deployment agent.

---
+++ install-code-deployment-agent.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# -------------------------------------- #
+# Install code-deployment agent
+# -------------------------------------- #
+
+REGION=$(ec2-metadata -z | awk -F": " '{print $2}' | sed -e 's/.$//g')
+
+wget https://aws-codedeploy-${REGION}.s3.amazonaws.com/latest/install
+chmod +x install
+
