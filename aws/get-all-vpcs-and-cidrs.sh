7c7d1cf6f6cb3f3fe97571fba7fe131aebd6b662
user <user@localhost>
2018-07-28 21:23:51 -0500

    Added aws folder, moved snippets arround, updated script to list all VPCs with their cirds.

---
+++ aws/get-all-vpcs-and-cidrs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+REGIONS=$(aws ec2 describe-regions --region us-west-2 --query "Regions[].RegionName" --output text| tr "\t" "\n" | tr "\n" " ")
+
+
+echo "Listing non default VPC's"
+for region in $REGIONS
+   do
+      echo "Getting Custom VPC list in $region region:"
+      aws ec2 describe-vpcs --filters Name=isDefault,Values=false --query "Vpcs[].{id:VpcId,cidr:CidrBlock}" --region=${region} --output table
+   done
+
+echo -e "-----\n------"
+
+echo "Listing default VPCS:"
+for region in $REGIONS
+   do
+      echo "Getting Custom VPC list in $region region:"
+      aws ec2 describe-vpcs --filters Name=isDefault,Values=false --query "Vpcs[].{id:VpcId,cidr:CidrBlock}" --region=${region} --output table
+   done
